162b4f635f9de947f476203763f59b8612a55f99
Bruno <fernando23296@gmail.com>
2018-10-19 16:57:56 -0400

    Adding a new file for filters

---
+++ change1.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np  
+from matplotlib import pyplot as plt
+
+img = cv2.imread('ex1.jpg')
+
+kernel = np.ones((5,5),np.uint8)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+erosion = cv2.erode(blurred,kernel,iterations=2)
+cv2.imshow("Image", erosion)
+cv2.waitKey(0)
+'''
+plt.subplot(122),plt.imshow(erosion),plt.title('erosion')
+plt.xticks([]), plt.yticks([])
+plt.show()
+'''
