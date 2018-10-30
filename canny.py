ec5b0faab3bf52dd2f60e95fe6f8c35fd70a7729
Bruno <fernando23296@gmail.com>
2018-10-29 23:14:51 -0400

    Adding a file for neural network research

--- canny.py
+++ canny.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('diag3_resize.png', 0)
-edges = cv2.Canny(img, 20, 70)
+img = cv2.imread('ex1.jpg', 0)
+edges = cv2.Canny(img, 0, 110)
 plt.subplot(121), plt.imshow(img, cmap='gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
