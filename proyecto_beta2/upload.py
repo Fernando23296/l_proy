4570456d80ffe98cecb4011c98c528666e671cdf
Bruno <fernando23296@gmail.com>
2018-11-11 12:34:19 -0400

    Now upload file is ready, works well

--- proyecto_beta2/upload.py
+++ proyecto_beta2/upload.py
@@ -2,8 +2,23 @@ import os
 
 from flask import Flask, request, render_template, send_from_directory
 from defs import *
+from def_plot import *
 import cv2
-
+from scipy.ndimage import rotate
+from scipy.misc import imread, imshow
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import interpolate
+import cv2
+import imutils
+from operator import is_not
+from functools import partial
+from pylab import *
+from random import *
+from sklearn.cluster import KMeans
+from matplotlib import transforms
+import scipy.ndimage.morphology as morp
+from skimage import feature
 __author__ = 'ibininja'
 
 app = Flask(__name__)
@@ -14,14 +29,6 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 @app.route("/")
 def index():
     return render_template("upload.html")
-
-
-def nombre_archivo(na):
-        punto = na.find(".")
-        b = ''
-        for i in range(0, punto):
-                b += str(na[i])
-        return b
 
 
 
@@ -43,18 +50,210 @@ def upload():
 
         ruta = 'static/'+filename
         img = cv2.imread(ruta, cv2.IMREAD_COLOR)
+        imagen2=img
         width = img.shape[1]
         print(width)
 
     print(filename)
-    extracto=nombre_archivo(filename)
-    complemento='_azul.png'
-    titulo_final=extracto+complemento
-    #filenamepro='/images/'+filename
-    #img = cv2.imread(filenamepro, cv2.IMREAD_COLOR)
-    #height = img.shape[0]
-    #print(height)
-    # return send_from_directory("images", filename, as_attachment=True)
+    
+    
+    
+    division = 13
+    height = img.shape[0]
+    width = img.shape[1]
+    qua = int(width/10)
+    qua2 = int(qua*3)
+    qua7 = int(qua*7)
+    img[0:height, 0:qua2] = [0]
+    img[0:height, qua7:width] = [0]
+
+    kernel = np.ones((5, 5), np.uint8)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 198, 300, cv2.THRESH_BINARY)[1]
+    edges2 = feature.canny(thresh, sigma=3)
+    skel = skeletonize(edges2)
+
+    extracto = nombre_archivo(filename)
+
+
+    nuevo_nombre = extracto+'_gts.png'
+    path = 'static/'
+
+    cv2.imwrite(os.path.join(path,nuevo_nombre), skel.astype(np.uint8)*255)
+
+
+    extracto = nombre_archivo(filename)
+    complemento = '_gts.png'
+    titulo_final = extracto+complemento
+
+    #______________________________________________________________________________________________
+
+
+    img = cv2.imread(os.path.join(path,titulo_final))
+    dimensions = img.shape
+    height = img.shape[0]
+    width = img.shape[1]
+    ancho = int(width)
+    altura = int(height)
+    alfa = int(altura/division)
+    cons = 0
+    a = np.empty(((division+1), 50), dtype=object)
+
+    for i in range(0, (division+1)):
+
+        cons1 = cons
+        cons2 = cons1+alfa
+        image = img[cons1:cons2, 0:ancho]
+
+        #convirtiendo a escala de grises
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #aplicando desenfoque gaussiano
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        #threshold?
+        thresh = cv2.threshold(blurred, 60, 200, cv2.THRESH_BINARY)[1]
+        cnts = cv2.findContours(
+            thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        count = 1
+        for c in cnts:
+            M = cv2.moments(c)
+
+            if M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+            else:
+                    cX, cY = 0, 0
+            xx = str(cX)+","+str(cY)
+            a[i][count] = [cX, cY]
+
+            cv2.drawContours(image, [c], -1, (0, 0, 255), 2)
+            #CIRCULO DE CENTRO
+            cv2.circle(image, (cX, cY), 7, (0, 0, 255), -1)
+            #COORDENADAS
+            cv2.putText(image, xx, (cX - 20, cY - 20),
+                        #TIPO DE LETRA, COLOR?
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            #cv2.imshow("Image", img)
+            count = count+1
+
+        cons = cons2
+        i = i+1
+
+    cero = [0, 0]
+    b = [[cero if x is None else x for x in c] for c in a]
+    #b = b[::-1]
+
+    b = igualador(b,division,alfa)
+
+    b = reemplazador(b, division)
+    ax = np.zeros(shape=((division+1), 1), dtype=object)
+    contador = 0
+
+    lis_2 = []
+
+    for i in range(0, (division+1)):
+        a = limpio(b[i])
+        lis_2.append(a)
+
+    lis_3 = []
+    lis_3 = rellenador(lis_2, division, ancho, altura)
+    ax = seleccionador_kmeans(lis_3)
+
+    #print("Lista: ", ax)
+
+    ancho2 = int(ancho/2)
+    axx = np.asarray(ax)
+    bx = np.array([[ancho2, altura]])
+    cx = np.concatenate((axx, bx), axis=0)
+    dx = np.array([[ancho2, 0]])
+
+    if (ax[0][1] < alfa):
+        axx = np.concatenate((dx, cx), axis=0)
+        axx = np.delete(axx, 1, 0)
+        axx = np.array(axx.T)
+    else:
+        axx = np.array(cx.T)
+
+    #print("VECTOR FINAL: ", axx)
+    #print("altura", altura)
+    #print("ancho", ancho)
+
+    dim = (ancho, altura)
+    resized = cv2.resize(imagen2, dim, interpolation=cv2.INTER_AREA)
+
+    fig, ax = plt.subplots()
+    
+    l_x = axx[0]
+    l_x = l_x.tolist()
+    l_y = axx[1]
+    l_y = l_y.tolist()
+
+    #plt.plot(l_x, l_y, 'ob')
+    #plt.plot(axx[0, :], axx[1, :], 'ob')
+    #plt.show()
+
+
+    #de aqui para abajo es todo girado
+    y = axx[0]
+    #print("_"*30)
+    #print(y)
+    #print("_"*30)
+    x = axx[1]
+    #print("_"*30)
+    #print(x)
+    #print("_"*30)
+
+
+    #print("___"*20)
+    pi1, pi2 = punto_inflexion(x, y)
+    #print("+++"*20)
+    a, b = max_min(x, y)
+    a_2 = a[1:3]
+    b_2 = b[1:3]
+    #print(a)
+    #print("___"*20)
+    #print(b)
+
+
+    z = np.polyfit(x, y, 5)
+    f = np.poly1d(z)
+
+    #print("Ecuacion bonita: ")
+    #print(f)
+    # calculate new x's and y's
+    x_new = np.linspace(x[0], x[-1], 50)
+    x_new2 = x_new[::-1]
+
+    y_new = f(x_new2)
+    y_new = y_new[::-1]
+
+    print(img_plot(x_new2,y_new,filename,fig,ax,a_2,b_2))
+    '''
+    
+    img2 = rotate(imagen2, -90)
+
+    width = altura
+
+    height = ancho
+    dim = (width, height)
+    resized = cv2.resize(img2, dim, interpolation=cv2.INTER_AREA)
+
+  
+    aa = ax.imshow(resized)
+
+
+    altura = img.shape[0]
+    width = img.shape[1]
+    y_grafo = y
+  
+    y_inv = y[::-1]
+
+    plt.plot(a_2, b_2, 'or')
+    '''
+
+
+
     return render_template("complete.html", image_name=titulo_final)
 
 
