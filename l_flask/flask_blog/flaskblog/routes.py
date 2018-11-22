a08fa38afcd07226f89500f983e7f131807fb636
Bruno <fernando23296@gmail.com>
2018-11-21 22:12:42 -0400

    Now account allows to change profile, username, email

--- l_flask/flask_blog/flaskblog/routes.py
+++ l_flask/flask_blog/flaskblog/routes.py
@@ -1,8 +1,13 @@
 from flask import  render_template, url_for, flash, redirect,request
 from flaskblog import app,db,bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User,Post
 from flask_login import login_user, current_user, logout_user,login_required
+import os
+import secrets
+from PIL import Image
+
+
 posts = [
     {
         'author': 'Corey Schafer',
@@ -66,8 +71,40 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    print (type(f_ext))
+    print(random_hex)
+    print(type(random_hex))
+    picture_fn = random_hex + f_ext 
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
-@app.route("/account")
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html',title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash('tu cuenta ha sido registrada', 'success')
+        return redirect(url_for('account'))
+    
+    elif request.method == 'GET':
+            form.username.data = current_user.username
+            form.email.data = current_user.email
+    image_file=url_for('static',filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html',title='Account', image_file=image_file, form = form)
