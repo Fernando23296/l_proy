2cdc15408916476bb9380d1264cb420b4be764ba
Bruno <fernando23296@gmail.com>
2018-11-24 17:39:14 -0400

    Allows to post a post, edit it, delete it and do a pagination

--- l_flask/flask_blog/flaskblog/forms.py
+++ l_flask/flask_blog/flaskblog/forms.py
@@ -1,10 +1,10 @@
-
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
-from flask_login import current_user
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -16,18 +16,16 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self,username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
-
         if user:
-            raise ValidationError('Ese usuario ya existe, por favor digite otro')
-
+            raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
-
         if user:
-            raise ValidationError('Ese email ya existe, por favor digite otro')
+            raise ValidationError('That email is taken. Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
@@ -42,19 +40,23 @@ class UpdateAccountForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-
-    picture =  FileField('Actualizar imagen de perfil', validators=[FileAllowed(['jpg','png'])])
-    submit = SubmitField('Actualizar')
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('Ese usuario ya fue tomado, elije otro')
+                raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
-
             if user:
-                raise ValidationError('Ese email ya existe, por favor digite otro')
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
